53c62f2ffa1267419be6575b51b199a5db3814b4
Prapassorn <uprapassorn@gmail.com>
2020-05-25 02:25:54 +0700

    change tag<keyword> to </keyword>

--- ReviewApplication/src/main/java/com/demo/review/repositories/ReviewRepository.java
+++ ReviewApplication/src/main/java/com/demo/review/repositories/ReviewRepository.java
@@ -17,7 +17,7 @@ public interface ReviewRepository extends JpaRepository<Review, Integer>{
 	@Query(value ="SELECT UUID() AS guid,reviewID,group_concat(review_text) AS review_text,1 AS seq from (select * FROM REVIEW WHERE reviewID = :id order by reviewID,seq)  group by reviewID",nativeQuery = true)
 	List<Review> findReviewById(@Param("id") Integer id);
 	
-	@Query(value ="SELECT UUID() AS guid,reviewID,replace(group_concat(review_text),:text,concat('<keyword>',:text,'<keyword>')) AS review_text,1 AS seq from (select * FROM REVIEW WHERE review_text like %:text% order by reviewID,seq)  group by reviewID",nativeQuery = true)
+	@Query(value ="SELECT UUID() AS guid,reviewID,replace(group_concat(review_text),:text,concat('<keyword>',:text,'</keyword>')) AS review_text,1 AS seq from (select * FROM REVIEW WHERE review_text like %:text% order by reviewID,seq)  group by reviewID",nativeQuery = true)
 	List<Review> findReviewByText(@Param("text") String text);
 	
 	Long deleteByReviewID(Integer id);
