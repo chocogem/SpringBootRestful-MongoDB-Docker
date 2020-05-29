6f30b6a91123772dcc029442420566c2ac397dcf
Prapassorn <uprapassorn@gmail.com>
2020-05-29 23:44:10 +0700

    Chang variable name

--- ReviewApplication/src/main/java/com/demo/review/ReviewApplication.java
+++ ReviewApplication/src/main/java/com/demo/review/ReviewApplication.java
@@ -1,6 +1,5 @@
 package com.demo.review;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
